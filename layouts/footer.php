1a0262aa7767b126a5492713c9a32874d3ea9cb3
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-09-08 20:55:07 -0400

    Initial

---
+++ layouts/footer.php
@@ -0,0 +1,20 @@
+	</div>
+	<footer>
+		<div id="footrow" class="row justify-content-center">
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h5>Even more about us</h5>
+				<hr class="foot-div">
+				<a class="footlink" href="about">About</a><br>
+				<a class="footlink" href="message_from_me">Message from the webmaster</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h5>Useful resources</h5>
+				<hr class="foot-div">
+				<a class="footlink" target="_blank" href="privacy_policy">Privacy Policy <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="footlink" target="_blank" href="how_to_make_a_site">I wanna make a site! <i class="fas fa-external-link-alt"></i></a>
+			</div>
+		</div>
+	</footer>
+	</div>
+	</body>
+</html>
