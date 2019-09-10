2ca5371ae668b93a0b24a8e1e87e3427961867b7
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-09-09 21:46:56 -0400

    Patch 1

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,19 +2,32 @@
 	<footer>
 		<div id="footrow" class="row justify-content-center">
 			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
-				<h5>Even more about us</h5>
+				<h6>Even more about us</h6>
 				<hr class="foot-div">
 				<a class="footlink" href="about">About</a><br>
 				<a class="footlink" href="message_from_me">Message from the webmaster</a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
-				<h5>Useful resources</h5>
+				<h6>Useful resources</h6>
+				<hr class="foot-div">
+				<a class="footlink" target="_blank" href="privacy_policy">Privacy Policy <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="footlink" target="_blank" href="how_to_make_a_site">I wanna make a site! <i class="fas fa-external-link-alt"></i></a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>Other cool stuff</h6>
+				<hr class="foot-div">
+				<a class="footlink" target="_blank" href="http://www.crhs.rsb.qc.ca">CRHS web site <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="footlink" target="_blank" href="http://www.crhsrobotics.com/2019">CRHS Robotics web site (2019) <i class="fas fa-external-link-alt"></i></a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>IDK</h6>
 				<hr class="foot-div">
 				<a class="footlink" target="_blank" href="privacy_policy">Privacy Policy <i class="fas fa-external-link-alt"></i></a><br>
 				<a class="footlink" target="_blank" href="how_to_make_a_site">I wanna make a site! <i class="fas fa-external-link-alt"></i></a>
 			</div>
 		</div>
 	</footer>
+	<button onclick="backToTop()" id="backtop"><i class="fas fa-caret-up"></i></button>
 	</div>
 	</body>
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -11,6 +11,7 @@
 		<meta name="keywords" content="CRHS, CRC, Robotics, CRC Robotics, south shore robotics, robotics south shore">
 		<meta name="author" content="Jacob-Mahmoud Alfahad">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -23,7 +24,7 @@
 				<a id="<?php if($title =='CRHS Robotics 2020 - ESPN Home'){echo 'active';}else{echo '';} ?>" href="index">Home</a>
 				<a id="<?php if($title =='CRHS Robotics 2020 - ESPN About'){echo 'active';}else{echo '';} ?>" href="about">About</a>
 				<div class="dropdown">
-				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Articles'){echo 'active';}else{echo '';} ?>" class="dropbtn">Articles <i class="fas fa-caret-down"></i></button>
+				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Articles'){echo 'active';}else{echo '';} ?>" class="dropbtn">Articles <i class="fas fa-caret-right"></i></button>
 				  <div class="dropdown-content">
 					<a href="#">Link 1</a>
 					<a href="#">Link 2</a>
@@ -31,7 +32,7 @@
 				  </div>
 				</div>
 				<div class="dropdown">
-				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Team'){echo 'active';}else{echo '';} ?>" class="dropbtn">Team <i class="fas fa-caret-down"></i></button>
+				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Team'){echo 'active';}else{echo '';} ?>" class="dropbtn">Team <i class="fas fa-caret-right"></i></button>
 				  <div class="dropdown-content">
 					<a href="#">Link 1</a>
 					<a href="#">Link 2</a>
@@ -43,7 +44,7 @@
 				<a id="<?php if($title =='CRHS Robotics 2020 - ESPN Home'){echo 'active';}else{echo '';} ?>" href="index">Home</a>
 				<a id="<?php if($title =='CRHS Robotics 2020 - ESPN About'){echo 'active';}else{echo '';} ?>" href="about">About</a>
 				<div class="dropdown">
-				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Articles'){echo 'active';}else{echo '';} ?>" class="dropbtn">Articles <i class="fas fa-caret-down"></i></button>
+				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Articles'){echo 'active';}else{echo '';} ?>" class="dropbtn">Articles <i class="fas fa-caret-right"></i></button>
 				  <div class="dropdown-content">
 					<a href="#">Link 1</a>
 					<a href="#">Link 2</a>
@@ -51,7 +52,7 @@
 				  </div>
 				</div>
 				<div class="dropdown">
-				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Team'){echo 'active';}else{echo '';} ?>" class="dropbtn">Team <i class="fas fa-caret-down"></i></button>
+				  <button id="<?php if($title =='CRHS Robotics 2020 - ESPN Team'){echo 'active';}else{echo '';} ?>" class="dropbtn">Team <i class="fas fa-caret-right"></i></button>
 				  <div class="dropdown-content">
 					<a href="#">Link 1</a>
 					<a href="#">Link 2</a>
