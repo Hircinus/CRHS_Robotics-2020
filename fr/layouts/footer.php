048d90c0afe7c74add2d81f48abb7d2915401e1e
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-10-08 21:09:10 -0400

    french
    started adding french stuff

---
+++ fr/layouts/footer.php
@@ -0,0 +1,35 @@
+	</div>
+	<footer>
+		<div id="footrow" class="row justify-content-center">
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>Un peu plus sur nous</h6>
+				<hr class="foot-div">
+				<a class="link" href="about">&Agrave; propos</a><br>
+				<a class="link" href="message_from_me">Message du cr&eacute;ateur du site</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>Ressources pertinentes</h6>
+				<hr class="foot-div">
+				<a class="link" target="_blank" href="privacy_policy">Privacy Policy <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="link" target="_blank" href="sitemap">Carte du site <i class="fas fa-external-link-alt"></i></a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>Autre choses int&eacute;r&eacute;ssant</h6>
+				<hr class="foot-div">
+				<a class="link" target="_blank" href="http://www.crhs.rsb.qc.ca">Site web de CRHS <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="link" target="_blank" href="http://www.crhsrobotics.com/2019">Site web de CRHS Robotique (2019) <i class="fas fa-external-link-alt"></i></a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12 col-12">
+				<h6>IDK</h6>
+				<hr class="foot-div">
+				<a class="link" target="_blank" href="privacy_policy">Privacy Policy <i class="fas fa-external-link-alt"></i></a><br>
+				<a class="link" target="_blank" href="how_to_make_a_site">I wanna make a site! <i class="fas fa-external-link-alt"></i></a>
+			</div>
+		</div>
+		<p class="text-center"><a class="link" href="humains.txt" target="_blank">humains.txt <i class="fas fa-external-link-alt"></i></a>&nbsp;
+      <a class="link" href="https://github.com/Hircinus/CRHS_Robotics-2020" target="_blank">GitHub <i class="fas fa-external-link-alt"></i></a></p>
+	</footer>
+	<button onclick="backToTop()" id="backtop"><i class="fas fa-caret-up"></i></button>
+	</div>
+	</body>
+</html>
