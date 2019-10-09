02c9764732a56ef1a005372695c603afacc900b3
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-10-08 21:02:47 -0400

    dark/light mode
     - Added toggleable dark/light mode
     - Added some fixes (not done) for dark mode
     - Small bug fixes
     - Started adding content

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,31 @@
+stylesheet = document.getElementById("stylesheet"); // DEFINE STYLESHEET
+lightbutton = document.getElementById("lightbutton"); // DEFINE LIGHT BUTTON
+darkbutton = document.getElementById("darkbutton"); // DEFINE DARK BUTTON
+theme = localStorage.getItem("theme"); // DEFINE THEME IDENTIFIER
+
+// DARK AND LIGHT THEME SWITCH
+function darktheme() {
+	stylesheet.href = "assets/style-dark.css";
+	localStorage.setItem("theme", "assets/style-dark.css");
+	console.log("Theme changed to dark.");
+	darkbutton.style.display = "none";
+	lightbutton.style.display = "block";
+}
+function lighttheme() {
+	stylesheet.href = "assets/style.css";
+	localStorage.setItem("theme", "assets/style.css");
+	console.log("Theme changed to light.");
+	lightbutton.style.display = "none";
+	darkbutton.style.display = "block";
+}
+if (localStorage.getItem("theme") === null) {
+	stylesheet.href = "assets/style.css";
+	localStorage.setItem("theme", "assets/style.css");
+}
+else {
+	stylesheet.href = theme; // CALL LOCAL STORAGE AND ASSIGN IT TO THEME
+}
+
 // When the user scrolls down x amount, make navbar appear
 window.onscroll = function() {scrollFunction()};
 
@@ -13,4 +41,5 @@ function scrollFunction() {
 function backToTop() {
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
+
 console.log("I hope that you aren't trying to do anything sneaky... ;)");
