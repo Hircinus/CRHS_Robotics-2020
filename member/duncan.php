b118d9e268ea870add93703f8696aae4f83cc27e
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-11-01 22:41:29 -0400

    Upload member stuff

---
+++ member/duncan.php
@@ -0,0 +1,9 @@
+<?php
+$member = 'Duncan Bruens-Home';
+$team = 'Programming';
+$years = '2<sup>nd</sup> year';
+$message = 'Yeet, I did it.';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
---
+++ member/jacob.php
@@ -0,0 +1,9 @@
+<?php
+$member = 'Jacob Alfahad';
+$team = 'Web';
+$years = '3<sup>rd</sup> year';
+$message = 'Yeet, I did it.';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
