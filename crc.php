4c8a7cf3d5f0c5bca0ac609614f72bc0b714cb8e
jovrobert2727 <32917220+jovrobert2727@users.noreply.github.com>
2019-11-18 20:07:00 -0500

    Add files via upload
    This is just the basic page layout applied for CRC.

---
+++ crc.php
@@ -0,0 +1,23 @@
+<?php
+$title = 'CRHS Robotics 2020 - ESPN About';
+include 'layouts/header.php';
+?>
+<div id="firstrow" class="row justify-content-center">
+   <div class="col-lg-10 col-md-11 col-sm-12 col-12">
+      <h2>CRC</h2>
+      <hr class="outer-divider">
+   </div>
+</div>
+<div id="row" class="row justify-content-around">
+   <div class="col-lg-9 col-md-10 col-sm-12 col-12">
+      <h3>School</h3>
+      <hr class="inner-divider">
+      <p>The CRC is the Canadian Robotics Competition.</p>
+      <ul>
+         <li>List item</li>
+      </ul>
+   </div>
+</div>
+<?php
+include 'layouts/footer.php';
+?>
