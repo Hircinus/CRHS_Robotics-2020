7109f5f353f9c2d26efa9fce59ab0d1392dc70c0
Jacob <36286032+Hircinus@users.noreply.github.com>
2019-12-16 00:35:30 -0500

    Fix
    Honestly I don't remember, all I know is that it looks better
     - Added cool transition for theme change

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,33 @@ stylesheet = document.getElementById("stylesheet"); // DEFINE STYLESHEET
 lightbutton = document.getElementById("lightbutton"); // DEFINE LIGHT BUTTON
 darkbutton = document.getElementById("darkbutton"); // DEFINE DARK BUTTON
 theme = localStorage.getItem("theme"); // DEFINE THEME IDENTIFIER
+transitionTheme = document.getElementById("transition");
 // DARK AND LIGHT THEME SWITCH
 function darktheme() {
-	stylesheet.href = "assets/style-dark.css";
-	localStorage.setItem("theme", "assets/style-dark.css");
-	console.log("Theme changed to dark.");
-	darkbutton.style.display = "none";
-	lightbutton.style.display = "block";
+	transitionTheme.style.display = 'block';
+	transitionTheme.addEventListener("animationend", darkthemeChange);
+	transitionTheme.addEventListener("webkitAnimationEnd", darkthemeChange);
+	function darkthemeChange() {
+		stylesheet.href = "assets/style-dark.css";
+		transitionTheme.style.display = 'none';
+		localStorage.setItem("theme", "assets/style-dark.css");
+		console.log("Theme changed to dark.");
+		darkbutton.style.display = "none";
+		lightbutton.style.display = "block";
+	}
 }
 function lighttheme() {
-	stylesheet.href = "assets/style.css";
-	localStorage.setItem("theme", "assets/style.css");
-	console.log("Theme changed to light.");
-	lightbutton.style.display = "none";
-	darkbutton.style.display = "block";
+	transitionTheme.style.display = 'block';
+	transitionTheme.addEventListener("animationend", lightthemeChange);
+	transitionTheme.addEventListener("webkitAnimationEnd", lightthemeChange);
+	function lightthemeChange() {
+		stylesheet.href = "assets/style.css";
+		transitionTheme.style.display = 'none';
+		localStorage.setItem("theme", "assets/style.css");
+		console.log("Theme changed to light.");
+		darkbutton.style.display = "block";
+		lightbutton.style.display = "none";
+	}
 }
 if (localStorage.getItem("theme") === null) {
 	stylesheet.href = "assets/style.css";
@@ -24,6 +37,7 @@ if (localStorage.getItem("theme") === null) {
 else {
 	stylesheet.href = theme; // CALL LOCAL STORAGE AND ASSIGN IT TO THEME
 }
+
 // When the user scrolls down x amount, make navbar appear
 window.onscroll = function() {scrollFunction()};
 
