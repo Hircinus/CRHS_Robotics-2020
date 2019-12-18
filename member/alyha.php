296370269c978715ebde97c8f3435415ecce58b1
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:48:23 -0500

    Update alyha.php

--- member/alyha.php
+++ member/alyha.php
@@ -1,1 +1,10 @@
-alyha.php
+<?php
+$member = 'Alyha';
+$team = 'Kiosk';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
+
