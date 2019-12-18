f3797e759a88c68ef6e8b6301dc5e5200f5f4c8f
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:41:37 -0500

    Update aafreen.php

--- member/aafreen.php
+++ member/aafreen.php
@@ -1,1 +1,9 @@
-aafreen.php
+<?php
+$member = 'Aafreen';
+$team = 'Kiosk';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
