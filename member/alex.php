e467b01213228a556ac15730c9ca4f2126f39f4a
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:47:35 -0500

    Update alex.php

--- member/alex.php
+++ member/alex.php
@@ -1,1 +1,10 @@
-alex.php
+<?php
+$member = 'Alex';
+$team = 'Robot';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
+
