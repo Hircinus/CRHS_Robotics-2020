45383c1ff796df3f7ccc48fdb8365d1ad18235aa
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:52:15 -0500

    Update benjamin_c.php

--- member/benjamin_c.php
+++ member/benjamin_c.php
@@ -1,1 +1,10 @@
-benjamin_c.php
+<?php
+$member = 'Benjamin C.';
+$team = 'Robot';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
+
