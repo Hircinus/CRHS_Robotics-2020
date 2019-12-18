bda83a8c23f0218ac2483f79234ee85060ec0516
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:58:07 -0500

    Update david_m.php

--- member/david_m.php
+++ member/david_m.php
@@ -1,1 +1,9 @@
-david_m.php
+<?php
+$member = 'David M.';
+$team = 'Robot';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
