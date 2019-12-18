15827502a4d886a051761ef32cb0f4dd5704cd5b
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:44:15 -0500

    Update adrien.php

--- member/adrien.php
+++ member/adrien.php
@@ -1,1 +1,9 @@
-adrien.php
+<?php
+$member = 'Adrien';
+$team = 'Robot';
+$years = '3<sup>rd</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
