c515f6767d16bed88dac0489a51e5c8be0bcd835
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:56:42 -0500

    Update charles.php

--- member/charles.php
+++ member/charles.php
@@ -1,1 +1,9 @@
-charles.php
+<?php
+$member = 'Charles';
+$team = 'Robot';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
