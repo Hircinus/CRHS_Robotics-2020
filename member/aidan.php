4c9287705e51eeef3b36af80962d13540d995bc9
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:46:21 -0500

    Update aidan.php

--- member/aidan.php
+++ member/aidan.php
@@ -1,1 +1,9 @@
-aidan.php
+<?php
+$member = 'Aidan';
+$team = 'Video';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
