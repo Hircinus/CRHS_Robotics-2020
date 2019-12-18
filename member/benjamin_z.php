499a2fa82037dbc0ac8f5652946ed7caf6a36b74
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:53:20 -0500

    Update benjamin_z.php

--- member/benjamin_z.php
+++ member/benjamin_z.php
@@ -1,1 +1,9 @@
-benjamin_z.php
+<?php
+$member = 'Benjamin Z.';
+$team = 'Web';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
