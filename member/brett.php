f3538fd8bf95c19d2c53572d211a35b6ca7ff0aa
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-18 18:54:42 -0500

    Update brett.php

--- member/brett.php
+++ member/brett.php
@@ -1,1 +1,9 @@
-brett.php
+<?php
+$member = 'Brett';
+$team = 'Video';
+$years = '2<sup>nd</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
