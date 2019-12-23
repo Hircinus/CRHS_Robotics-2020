7b8091486f56291f3da6113b0c398affa24094d4
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:46:50 -0500

    Update inaya.php

--- member/inaya.php
+++ member/inaya.php
@@ -1,1 +1,9 @@
-inaya.php
+<?php
+$member = 'Inaya';
+$team = 'Kiosk';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
