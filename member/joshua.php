833e46f84c7f2b089f56e0137b3bbe4fe00eb04c
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:52:22 -0500

    Update joshua.php

--- member/joshua.php
+++ member/joshua.php
@@ -1,1 +1,9 @@
-joshua.php
+<?php
+$member = 'Joshua';
+$team = 'Robot';
+$years = '2<sup>nd</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
