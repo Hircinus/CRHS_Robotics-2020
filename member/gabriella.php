c1076efb1357444bdf10c2e84d3c354821e8f36c
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:45:40 -0500

    Update gabriella.php

--- member/gabriella.php
+++ member/gabriella.php
@@ -1,1 +1,9 @@
-gabriella.php
+<?php
+$member = 'Gabriella';
+$team = 'Kiosk';
+$years = '2<sup>nd</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
