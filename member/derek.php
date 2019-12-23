27ef9914f46899200dc79fc53d75b83986fb22fa
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:39:12 -0500

    Update derek.php

--- member/derek.php
+++ member/derek.php
@@ -1,1 +1,9 @@
-derek.php
+<?php
+$member = 'Derek';
+$team = 'Video';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
