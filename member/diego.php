e8fcd8361bb468d026705e91b0bae86ed242c2ee
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:41:15 -0500

    Update diego.php

--- member/diego.php
+++ member/diego.php
@@ -1,1 +1,9 @@
-diego.php
+<?php
+$member = 'Diego';
+$team = 'Video';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
