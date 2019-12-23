539e2f9fa28b1ac648df6e8fb7c7e9fbd59cea37
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:42:22 -0500

    Update emily.php

--- member/emily.php
+++ member/emily.php
@@ -1,1 +1,9 @@
-emily.php
+<?php
+$member = 'Emily';
+$team = 'Kiosk';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
