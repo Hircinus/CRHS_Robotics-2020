0eee842159543281cdf29c85fa97539de5eb3468
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:44:21 -0500

    Update filippo.php

--- member/filippo.php
+++ member/filippo.php
@@ -1,1 +1,9 @@
-filippo.php
+<?php
+$member = 'Filippo';
+$team = 'Robot';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
