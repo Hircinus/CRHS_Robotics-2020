e2c2e9eda4d15d63cf2906ef52c2c428c301c583
6165203 <56939887+6165203@users.noreply.github.com>
2019-12-23 17:38:00 -0500

    Update david_t.php

--- member/david_t.php
+++ member/david_t.php
@@ -1,1 +1,9 @@
-david_t.php
+<?php
+$member = 'David T.';
+$team = 'Web';
+$years = '1<sup>st</sup> year';
+$message = ' ';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
