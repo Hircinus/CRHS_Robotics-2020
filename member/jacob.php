e8b635998bd2022868b07b17e5e0a5516dfdba7e
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-01-02 09:35:48 -0500

    Minor
    Little additions, not complete yet

--- member/jacob.php
+++ member/jacob.php
@@ -1,7 +1,10 @@
 <?php
+$title = "Jacob Alfahad / Web - CRHS Robotics 2020";
 $member = 'Jacob Alfahad';
+$profileLink = "jacob.jpg";
 $team = 'Web';
 $years = '3<sup>rd</sup> year';
+$grade = "Sec. 5";
 $message = 'Yeet, I did it.';
 require '../layouts/article-header.php';
 require '../layouts/team_member.php';
--- member/jovan.php
+++ member/jovan.php
@@ -1,1 +1,12 @@
-jovan.php
+<?php
+$title = "Jovan Gavranovic / Web - CRHS Robotics 2020";
+$member = 'Jovan Gavranovic';
+$profileLink = "jovan.jpg";
+$team = 'Web';
+$years = '2<sup>nd</sup> year';
+$grade = "Sec. 4";
+$message = 'Text.';
+require '../layouts/article-header.php';
+require '../layouts/team_member.php';
+require '../layouts/article-footer.php';
+?>
