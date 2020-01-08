7443847bbd155ea83fa8a074b83fbb3aacd42941
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-01-07 21:11:27 -0500

    Fix up french stuff
    To allow more easy french redirects, all french files must have the same names as english ones

---
+++ fr/home.php
@@ -0,0 +1,74 @@
+<?php
+$title = "CRHS Robotique 2020 - ESPN Accueil";
+require 'layouts/header.php';
+?>
+<!--
+<div id="homerow" class="row justify-content-center align-items-center">
+	<div class="col-lg-7 col-md-12 col-sm-12 col-12">
+		<h1>
+      CRHS
+      <br>
+      ROBOTIQUE
+      </h1>
+      <hr>
+      <h2>&Eacute;dition 2020</h2>
+	</div>
+	<div class="col-lg-5 col-md-12 col-sm-12 col-12">
+      <a class="btnlink" href="about">Qui sommes-nous?</a>
+      <a class="btnlink" href="team">Notre &eacute;quipe</a>
+	</div>
+</div>
+-->
+<div id="row" class="row justify-content-center">
+   <div id="side-card" class="col-lg-3 col-md-0 col-sm-0 col-0">
+      <h3>Quelques lonks</h3>
+      <ul class="no_bullet">
+         <li><a class="link" href="#">Premi&egrave;re rencontre: c'est quoi le plan de jeu?</a></li>
+         <li><a class="link" href="#">Robot team's thoughts</a></li>
+         <li><a class="link" href="#">Video team's thoughts</a></li>
+         <li><a class="link" href="#">Kiosk team's thoughts</a></li>
+      </ul>
+   </div>
+	<div id="article" class="col-lg-6 col-md-9 col-sm-12 col-12">
+		<div class="card">
+		  <a href="article/01"><img class="img-fluid lazy" src="../assets/loading.gif" data-src="../assets/stock.jpg" alt="Card image cap"></a>
+		  <div class="card-body tech-article">
+			<a href="article/tech" class="tag">Tech</a>
+			<h5 class="card-title">Une article</h5>
+			<p class="card-text">Cool text to make you wanna learn more about something.</p>
+			<a href="article/01" class="link">Continuer la lecture</a>
+		  </div>
+		</div>
+	</div>
+   <div class="col-lg-3 col-md-0 col-sm-0 col-0">
+   </div>
+</div>
+<div id="row" class="row justify-content-center">
+	<div id="article" class="col-lg-6 col-md-9 col-sm-12 col-12">
+		<div class="card">
+		  <a href="article/1"><img class="img-fluid lazy" src="../assets/loading.gif" data-src="../assets/stock.jpg" alt="Card image cap"></a>
+		  <div class="card-body important-article">
+			<a href="article/important" class="tag">Important</a>
+			<h5 class="card-title">An article</h5>
+			<p class="card-text">Cool text to make you wanna learn more about something.</p>
+			<a href="article/12" class="link">Continuer la lecture</a>
+		  </div>
+		</div>
+	</div>
+</div>
+<div id="lastrow" class="row justify-content-center">
+	<div id="article" class="col-lg-6 col-md-9 col-sm-12 col-12">
+		<div class="card">
+		  <a href="article/1"><img class="img-fluid lazy" src="../assets/loading.gif" data-src="../assets/stock.jpg" alt="Card image cap"></a>
+		  <div class="card-body important-article">
+			<a href="article/important" class="tag">Important</a>
+			<h5 class="card-title">An article</h5>
+			<p class="card-text">Cool text to make you wanna learn more about something.</p>
+			<a href="article/11" class="link">Continuer la lecture</a>
+		  </div>
+		</div>
+	</div>
+</div>
+<?php
+require 'layouts/footer.php';
+?>
