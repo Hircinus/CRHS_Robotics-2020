72b1edad5e2f6ca5874496147a667aa79e14b912
JinxQc <32917220+JinxQc@users.noreply.github.com>
2020-01-11 23:48:46 -0500

    Add files via upload

--- team.php
+++ team.php
@@ -10,71 +10,141 @@ require 'layouts/header.php';
 	</div>
 </div>
 <a name="Robot"></a>
-<div id="row" class="row justify-content-center">
+<div id="row" class="row justify-content-center list">
 	<div class="col-9">
 		<h2>Robot</h2>
 		<hr class="inner-divider">
-		<div class="col-lg-3 col-md-3 col-sm-9 col-9">
-			<ul class="no_bullet">
-				<li><a href="member/adrien" class="link">Adrien</a></li>
-				<li><a href="member/alex" class="link">Alex</a></li>
-				<li><a href="member/benjamin_c" class='link'>Benjamin C</a></li>
-			</ul>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-9 col-9">
-			<ul class="no_bullet">
-				<li><a href="member/charles" class="link">Charles</a></li>
-				<li><a href="member/david_m" class="link">David M</a></li>
-				<li><a href="member/filippo" class="link">Filippo</a></li>
-			</ul>
-		</div>
-		<div class="col-lg-3 col-md-3 col-sm-9 col-9">
-			<ul class="no_bullet">
-				<li><a href="member/isa" class="link">Isa</a></li>
-				<li><a href="member/joshua" class="link">Joshua</a></li>
-				<li><a href="member/luke" class="link">Luke</a></li>
-			</ul>
-		</div>
+		<div class="row justify-content-center">
+            <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                <ul class="no_bullet">
+                    <li><a href="member/adrien" class="link">Adrien</a></li>
+                    <li><a href="member/alex" class="link">Alex</a></li>
+                    <li><a href="member/benjamin_c" class='link'>Benjamin C.</a></li>
+                    <li><a href="member/charles" class="link">Charles</a></li>
+                    <li><a href="member/david_m" class="link">David M.</a></li>
+                    <li><a href="member/filippo" class="link">Filippo</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                <ul class="no_bullet">
+                    <li><a href="member/isa" class="link">Isa</a></li>
+                    <li><a href="member/joshua" class="link">Joshua</a></li>
+                    <li><a href="member/luke" class="link">Luke</a></li>
+                    <li><a href="member/mallory" class="link">Mallory</a></li>
+                    <li><a href="member/mariam" class="link">Mariam</a></li>
+                    <li><a href="member/muhammad" class="link">Muhammad</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                <ul class="no_bullet">           
+                    <li><a href="member/qasim" class="link">Qasim</a></li>
+                    <li><a href="member/rakesh" class="link">Rakesh</a></li>
+                    <li><a href="member/ravi" class="link">Ravi</a></li>
+                    <li><a href="member/sofia" class="link">Sofia</a></li>
+                    <li><a href="member/tommy" class="link">Tommy</a></li>
+                    <li><a href="member/walid" class="link">Walid</a></li>
+                </ul>
+            </div>
+        </div>
 	</div>
 </div>
 <a name="Kiosk"></a>
 <div id="row" class="row justify-content-center">
-	<div class="col-10">
+	<div class="col-9">
 		<h2>Kiosk</h2>
 		<hr class="inner-divider">
-		<ul class="no_bullet">
-			<li><a href="member/aafreen" class="link">Aafreen</a></li>
-			<li><a href="member/alyha" class="link">Alyha</a></li>
-			<li><a href="member/benjamin_c" class="link">Benjamin C.</a></li>
-		</ul>
+            <div class="row justify-content-center">
+                <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                    <ul class="no_bullet">
+                    <li><a href="member/aafreen" class="link">Aafreen</a></li>
+                    <li><a href="member/alyha" class="link">Alyha</a></li>
+                    <li><a href="member/emily" class="link">Emily</a></li>
+                    <li><a href="member/gabriella" class="link">Gabriella</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                    <ul class="no_bullet">
+                    <li><a href="member/inaya" class="link">Inaya</a></li>
+                    <li><a href="member/jana" class="link">Jana</a></li>
+                    <li><a href="member/letitia" class="link">Letitia</a></li>
+                    <li><a href="member/marisa" class="link">Marisa</a></li>
+                    <li><a href="member/natalia" class="link">Natalia</a></li>
+                    </ul>    
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                  <ul class='no_bullet'>
+                    <li><a href="member/rayann" class="link">Rayann</a></li>
+                    <li><a href="member/shaniya" class="link">Shaniya</a></li>
+                    <li><a href="member/zackary_m" class="link">Zackary M.</a></li>
+                    <li><a href="member/zackary_r" class="link">Zackary R.</a></li>
+                    </ul>
+              </div>
+            </div>
 	</div>
 </div>
 <a name="Video"></a>
 <div id="row" class="row justify-content-center">
-	<div class="col-10">
+	<div class="col-9">
 		<h2>Video</h2>
 		<hr class="inner-divider">
-		<ul class="no_bullet">
-			<li><a href="member/aidan" class="link">Aidan</a></li>
-			<li><a href="member/amaan" class="link">Amaan</a></li>
-		</ul>
+		<div class="row justify-content-center">
+                <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                    <ul class="no_bullet">
+                    <li><a href="member/aidan" class="link">Aidan</a></li>
+                    <li><a href="member/brett" class="link">Brett</a></li>
+                    <li><a href="member/derek" class="link">Derek</a></li>
+                    <li><a href="member/diego" class="link">Diego</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                    <ul class="no_bullet">
+                    <li><a href="member/indra" class="link">Indra</a></li>
+                    <li><a href="member/julia" class="link">Julia</a></li>
+                    <li><a href="member/magnus" class="link">Magnus</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+                    <ul class="no_bullet">
+                    <li><a href="member/maximus" class="link">Maximus</a></li>
+                    <li><a href="member/nicholas" class="link">Nicholas</a></li>
+                    <li><a href="member/ryan" class="link">Ryan</a></li>
+                    <li><a href="member/samir" class="link">Samir</a></li>
+                    </ul>
+                </div>
+            </div>
 	</div>
 </div>
 <a name="Web"></a>
 <div id="row" class="row justify-content-center">
-	<div class="col-10">
+	<div class="col-9">
 		<h2>Web/Journalism</h2>
 		<hr class="inner-divider">
+        <div class='row justify-content-center'>
+        <div class='col-lg-3 col-md-3 col-sm-9 col-9'>
 		<ul class="no_bullet">
+            <li><a href="member/david_l" class="link">David L</a></li>
+            <li><a href="member/benjamin_z" class="link">Benjamin Z.</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-9 col-9">  
+            <ul class="no_bullet">
+            <li><a href="member/eshal" class="link">Eshal</a></li>
 			<li><a href="member/jacob" class="link">Jacob</a></li>
 			<li><a href="member/jovan" class="link">Jovan</a></li>
-			<li><a href="member/benjamin_z" class="link">Benjamin Z.</a></li>
-		</ul>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-9 col-9">
+            <ul class="no_bullet">
+            <li><a href="member/nadia" class="link">Nadia</a></li>
+            <li><a href="member/walid_c" class="link">Walid C.</a></li>
+            </ul>
+        </div>
+        </div>
 	</div>
 </div>
 <a name="Mentors"></a>
 <div id="row" class="row justify-content-center">
-	<div class="col-10">
+	<div class="col-9">
 		<h2>Mentors</h2>
 		<hr class="inner-divider">
 	</div>
