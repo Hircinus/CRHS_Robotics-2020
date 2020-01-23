bd11ab538285f0e8dce46d822e45b85d4880caae
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-01-23 14:38:40 -0500

    Fix up article tags

--- layouts/article.php
+++ layouts/article.php
@@ -3,8 +3,14 @@
 		<div id="firstrow" class="row bg-light-j">
 			<div class="col-12">
 				<h2 class="article-title"><?php echo $articletitle ?></h2>
-				<h3 class="article-tag"><a href="../articles?category=<?php echo strtolower($tag) ?>" class="<?php if($tag == 'Tech') { echo 'blue'; } if($tag == 'Important') { echo 'green'; } else { echo ''; } ?>"><?php echo $tag ?></a></h3>
-				<a class="link" href="../member/<?php echo $authorLink ?>"><?php echo $author ?></a>
+				<h3 class="article-tag"><a href="../articles?category=<?php echo strtolower($tag) ?>" class="<?php
+				if($tag == 'Tech') { echo 'orange'; }
+				elseif($tag == 'robot') { echo 'black'; }
+				elseif($tag == 'Tech') { echo 'blue'; }
+				elseif($tag == 'Important') { echo 'green'; }
+				elseif($tag == 'kiosk') { echo 'red'; }
+				else { echo ''; } ?>"><?php echo $tag ?></a></h3>
+				<a class="link" href="../member/<?php echo strtolower($author) ?>"><?php echo $author ?></a>
         <hr class="article-title-hr">
 			</div>
 		</div>
