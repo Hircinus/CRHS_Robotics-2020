65ea1af6bf7e03c13b2f3e5c45825d9b677eaa44
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-01-24 16:58:06 -0500

    Add files via upload

--- layouts/team_member.php
+++ layouts/team_member.php
@@ -1,10 +1,11 @@
 <div id="firstrow" class="row align-items-middle">
   <div class="col-2">
-    <img src="assets/<?php echo $profileLink ?>" class="profile">
+    <img src="/crhsrobotics2020/member/assets/<?php echo $profileLink ?>" class="profile">
   </div>
   <div class="col-10">
-    <h1><?php echo $member ?></h1>
+    <h1><?php echo $member ?><span class="id"><?php echo $number ?></span></h1>
     <hr class="outer-divider">
+    <h2 class="text-medium"><?php echo $role ?></h2>
     <h2 class="text-medium"><?php echo $team ?> &bull; <?php echo $years ?> &bull; <?php echo $grade ?></h2>
     <p><?php echo $message ?></p>
   </div>
@@ -15,11 +16,10 @@
     <p>
     <?php
     if($team == 'Robot'){ echo 'Robot teammates'; }
-    if($team == 'Kiosk'){ echo 'Kiosk teammates'; }
-    if($team == 'Programming'){ echo 'Programming teammates'; }
-    if($team == 'Web'){ echo 'Web teammates'; }
-    if($team == 'Journalism'){ echo 'Journalism teammates'; }
-    if($team == 'Mentor'){ echo 'Mentor teammates'; }
+    elseif($team == 'Kiosk'){ echo 'Kiosk teammates'; }
+    elseif($team == 'Programming'){ echo 'Programming teammates'; }
+    elseif($team == 'Web/journalism'){ echo '<a href="../team#Web" class="link">Web teammates</a>'; }
+    elseif($team == 'Mentor'){ echo 'Mentor teammates'; }
     ?>
   </div>
 </div>
