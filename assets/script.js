9dd1b296d333d5cee748fa5d910ca407ff0a1073
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-01-27 23:00:01 -0500

    Final (hopefully)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
+// If window width is equal to or less than 1150px,
+// scroll user down to content after navbar
+if(window.innerWidth <= 1150) {
+  if(location.hash == "") {
+    location.hash = "#target";
+    location.reload();
+  }
+}
 // When the user scrolls down x amount, make navbar appear
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 105) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("navbar").style.top = "0";
 	document.getElementById("backtop").style.right = "50px";
   } else {
@@ -12,60 +20,6 @@ function scrollFunction() {
 }
 function backToTop() {
   window.scrollTo({top: 0, behavior: 'smooth'});
-}
-
-// Category switching on "/articles.php"
-var important_button = document.getElementById('important_button');
-var tech_button = document.getElementById('tech_button');
-// function to retrieve cookie (generated with PHP based on $_GET[] value)
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-// Check cookie value against possible categories and displays appropriate ones
-if(getCookie("cat") == "tech") {
-  // Remove important
-  var important = document.getElementById("important-container");
-  important.style.display = "none";
-  important_button.classList.remove("toggled");
-}
-if(getCookie("cat") == "important") {
-  // Remove tech
-  var tech = document.getElementById("tech-container");
-  tech.style.display = "none";
-  tech_button.classList.remove("toggled");
-}
-// Display categories based on "onclick" event
-function showImportant() {
-  var important = document.getElementById("important-container");
-  if (important.style.display === "none") {
-    important.style.display = "block";
-    important_button.classList.add("toggled");
-  } else {
-    important.style.display = "none";
-    important_button.classList.remove("toggled");
-  }
-}
-function showTech() {
-  var tech = document.getElementById("tech-container");
-  if (tech.style.display === "none") {
-    tech.style.display = "block";
-    tech_button.classList.add("toggled");
-  } else {
-    tech.style.display = "none";
-    tech_button.classList.remove("toggled");
-  }
 }
 
 // Console message to show we're cool B)
